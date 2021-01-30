353cc016554a9f2b793ac120a8795fe2db89039b
emilymcgivern <emily.mcgivern3@mail.dcu.ie>
2021-01-30 21:07:59 +0000

    removing original scraper file

--- src/spiders/tesco_spider.py
+++ src/spiders/tesco_spider.py
@@ -11,11 +11,5 @@ class TescoSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
         page_content = response.xpath('//li/text()').getall()
-        # test = response.xpath('//li/text()').get(): selector.xpath('@href').get()
         self.log(page_content)
-        # filename = f'quotes-{page}.html'
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log(f'Saved file {filename}')
