197483fc014619fcc564f37bdfa42a08c7151bc1
Emily McGivern <emily.mcgivern3@mail.dcu.ie>
2021-01-30 21:11:53 +0000

    Feature: Adding basic scraper functionality (#1)
    * feat(accidentally-vegan): adding basic functionality to scrape grocery homepage
    
    * adding scrapy framework and basic spider
    
    * cleaning up hidden files and adding to gitignore
    
    * updating .gitignore
    
    * yet another .gitignore update
    
    * removing original scraper file
    
    * updating scraper name for running command

---
+++ src/spiders/tesco_spider.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class TescoSpider(scrapy.Spider):
+    name = "tesco"
+
+    def start_requests(self):
+        urls = [
+            'https://www.tesco.ie/groceries/department/default.aspx?N=4294954027&Ne=4294954028' #fresh food
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page_content = response.xpath('//li/text()').getall()
+        self.log(page_content)
